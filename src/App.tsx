7e130b47aa1ff86cfebc9d3530e13aded9d226c2
SUNAMI SWAYAMPRAVA MOHANTY <mohantysunamiswayamprava@gmail.com>
2025-08-20 20:02:00 +0530

    add tailwind with react commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { WelcomePage } from './pages/welcomePage';
+// import { LoginPage } from './pages/loginPage';
+// import { CreateAccountPage } from './pages/CreateAccountPage';
+// import { AccountSettingsPage } from './pages/AccountSettingsPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-[#f7f8f9] flex items-center justify-center p-4">
+        <div className="w-[455px] bg-[#c4c3c325] min-h-screen shadow-lg">
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            {/* <Route path="/login" element={<LoginPage />} />
+            <Route path="/create-account" element={<CreateAccountPage />} />
+            <Route path="/account-settings" element={<AccountSettingsPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} /> */}
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
